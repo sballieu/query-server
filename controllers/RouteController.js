db650602a0b571bfb990615a3d8f0a76d462feda
Pieter Colpaert <pieter.colpaert@okfn.org>
2016-04-29 18:32:29 +0200

    Added default timezone

--- controllers/RouteController.js
+++ controllers/RouteController.js
@@ -1,12 +1,12 @@
-var csa = require('csa');
+var csa = require('csa'),
+    moment = require('moment-timezone');
 
 module.exports = function (request, response, next) {
   if (!request.query.departureTime) {
     response.status(400).send('Please provide us with a parseable ISO8601 departureTime as a GET parameter');
   } else {
-    
     var planner = new csa.BasicCSA({departureStop: request.query.departureStop, arrivalStop: request.query.arrivalStop,departureTime: new Date(request.query.departureTime)});
-    var departureTime = new Date(request.query.departureTime);
+    var departureTime = moment.tz(request.query.departureTime,request.locals.config.defaultTimezone || 'Europe/Brussels');
     var connectionsStream = request.db.getConnectionsStream(departureTime);
     connectionsStream.on('error', function (error) {
       next('MongoDB error ' + error);
