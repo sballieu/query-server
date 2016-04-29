cc4a60f97eb98b2ba01eb5bb837226203ef1197c
Pieter Colpaert <pieter.colpaert@okfn.org>
2016-04-29 18:38:14 +0200

    Javascript date

--- controllers/RouteController.js
+++ controllers/RouteController.js
@@ -5,8 +5,9 @@ module.exports = function (request, response, next) {
   if (!request.query.departureTime) {
     response.status(400).send('Please provide us with a parseable ISO8601 departureTime as a GET parameter');
   } else {
-    var planner = new csa.BasicCSA({departureStop: request.query.departureStop, arrivalStop: request.query.arrivalStop,departureTime: new Date(request.query.departureTime)});
     var departureTime = moment.tz(request.query.departureTime,request.locals.config.defaultTimezone || 'Europe/Brussels');
+    departureTime = departureTime.toDate();
+    var planner = new csa.BasicCSA({departureStop: request.query.departureStop, arrivalStop: request.query.arrivalStop,departureTime: departureTime});
     var connectionsStream = request.db.getConnectionsStream(departureTime);
     connectionsStream.on('error', function (error) {
       next('MongoDB error ' + error);
